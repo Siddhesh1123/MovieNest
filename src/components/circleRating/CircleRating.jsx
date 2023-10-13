328761371ac8ac0e61739a7ac874877f4eb2f217
Siddhesh1123 <101471387+Siddhesh1123@users.noreply.github.com>
2023-10-13 23:33:16 +0530

    changes

--- src/components/circleRating/CircleRating.jsx
+++ src/components/circleRating/CircleRating.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import "./style.scss"
 
-import "./style.scss";
 
 const CircleRating = ({rating}) => {
   return (
